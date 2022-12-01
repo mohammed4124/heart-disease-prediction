ffad3e691e68d1285118fe1bea08553b80bcd68e
boutarfamedamin <boutarfamedamin@gmail.com>
2022-12-01 17:03:42 +0100

    modify the main app file

--- application.py
+++ application.py
@@ -124,6 +124,7 @@ def test(test_values):
 
 
 def plot_knn_scores():
+    
     if knn_scores==[]:
         dataProcessingAndAlgorithm()
     plt.plot([k for k in range(1, 21)], knn_scores, color='blue')
